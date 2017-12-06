fe680419a43cf617f0d2649a2a4329902935f856
Sahejpreet Sandhu <indango@gmail.com>
2017-12-06 11:09:04 -0800

    Checkpoint 5 address bloc menu: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,14 +8,14 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-     index = 0
-     entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
 end
