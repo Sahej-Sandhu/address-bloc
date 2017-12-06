2a0d6977a0286ae2c111a102a8ef2cd4f7561a89
Sahejpreet Sandhu <indango@gmail.com>
2017-12-05 17:02:57 -0800

    Assignment 4 Address Bloc: Module

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,7 +8,7 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-     index = 0
+    index = 0
      entries.each do |entry|
        if name < entry.name
          break
@@ -16,6 +16,16 @@ class AddressBook
        index+= 1
      end
      entries.insert(index, Entry.new(name, phone_number, email))
-   end
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+    @entries.each do |entry|
+      if(name == entry.name && phone_number == entry.phone_number && email == entry.email)
+        delete_entry = entry
+      end
+    end
+    @entries.delete(delete_entry)
+  end
 
 end
